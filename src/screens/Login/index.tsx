786bf5de4774e0d88c15437e96a9d0d9cbe8fa79
Connie <conniedpaula@gmail.com>
2025-12-05 15:43:14 -0300

    feature: logo disappears when inputs are focused

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,12 +1,14 @@
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 
 import { DismissKeyboardView } from "@/components/DismissKeyboardView";
 import { LoginForm } from "./LoginForm";
+import { AuthHeader } from "@/components/AuthHeader";
 
 export const Login = () => {
   return (
     <DismissKeyboardView>
       <View className="flex-1 w-[82%] self-center">
+        <AuthHeader />
         <LoginForm />
       </View>
     </DismissKeyboardView>
