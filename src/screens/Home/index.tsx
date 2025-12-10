7ed70ee1bbf8653a1f1c31341111ef8d56fa63d2
Connie <conniedpaula@gmail.com>
2025-12-10 11:47:13 -0300

    chore: update home and app header before working on transaction listing

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,10 +2,12 @@ import { AppHeader } from "@/components/AppHeader";
 import { useTransactionContext } from "@/context/transaction.context";
 import { useErrorHandler } from "@/shared/hooks/useErrorHandler";
 import { useEffect } from "react";
+import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ListHeader } from "./ListHeader";
 
 export const Home = () => {
-  const { fetchCategories } = useTransactionContext();
+  const { fetchCategories, fetchTransactions } = useTransactionContext();
   const { handleError } = useErrorHandler();
 
   const handleFetchCategories = async () => {
@@ -18,13 +20,18 @@ export const Home = () => {
 
   useEffect(() => {
     (async () => {
-      await handleFetchCategories();
+      await Promise.all([handleFetchCategories(), fetchTransactions()]);
     })();
   }, []);
-  
+
   return (
     <SafeAreaView className="flex-1 bg-background-primary">
-      <AppHeader />
+      <FlatList
+        data={[]}
+        renderItem={() => <></>}
+        ListHeaderComponent={ListHeader}
+        className="bg-background-secondary"
+      />
     </SafeAreaView>
   );
 };
